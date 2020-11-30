2d76569a556b457f00b17f6187f921120407807a
jakob lumpkin <lumpkinjakobr@gmail.com>
2020-11-30 12:35:51 -0600

    final

--- bankaccount/settings.py
+++ bankaccount/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'register.apps.RegisterConfig',
     'user',
-    'register',
+    # 'register',
 ]
 
 MIDDLEWARE = [
--- register/views.py
+++ register/views.py
@@ -3,13 +3,22 @@ from .forms import RegisterForm
 from user.models import account
 # Create your views here.
 def register(request):
+    # if request.method=="POST":
+    #     form=RegisterForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         username = request.POST['username']
+    #         account.objects.create(username=username, balance=0.0)
+    #         redirect('/')
+    # else:
+    form = RegisterForm()
+    return render(request, "register/register.html", {"form":form})
+
+def registerhome(request):
     if request.method=="POST":
         form=RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = request.POST['username']
             account.objects.create(username=username, balance=0.0)
-            redirect("/")
-    else:
-        form = RegisterForm()
-    return render(request, "register/register.html", {"form":form})
+    return redirect('/')
